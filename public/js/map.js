4c665c471a974f01666b9292e8af4845a7909020
Kaushal Chaudhary <kaushalc361@gmail.com>
2024-05-30 11:07:28 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken =mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map', 
+    center: coordinates, 
+    zoom: 9 
+});
+
+const marker=new mapboxgl.Marker()
+.setLngLat(coordinates)
+.addTo(map);
